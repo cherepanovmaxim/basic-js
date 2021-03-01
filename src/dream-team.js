0a3bfcce4d46c0bbedd65b9ff6ffe9d5ef169fa3
cherepanovmaxim <42759942+cherepanovmaxim@users.noreply.github.com>
2021-03-01 17:17:41 -0500

    fix: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,39 +1,19 @@
-
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(members ) {
-  
-  
-  let final = ''
+module.exports = function createDreamTeam(members) {
+  if (members == undefined) {
+    return false;
+  } else if (Array.isArray(members) == false) {
+    return false;
+  } else {
+    const ready = members
+      .filter((e) => typeof e === "string")
+      .map((e) => e.trim())
+      .map((e) => e[0])
+      .map((e) => e.toUpperCase())
+      .sort()
+      .join("");
 
-let array =[]; 
-for (let i=0; i<members.length; i++) {
-  
-  
-  
-  if(  typeof(members[i]) === 'string' ) {
-
-
-  array.push(members[i].toUpperCase())
-
-
-
-
-
+    return ready;
   }
-
-  
-  
-}
-
-
-
-    for(let i = 0; i<array.length; i++) {
-
-
-      final+= array.sort()[i].charAt(0)
-    }
-
-return final;
-  
 };
